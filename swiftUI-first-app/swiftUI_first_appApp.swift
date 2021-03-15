9c581bc99294a12c1892f7fa814664da0c2cafec
smilemike <419854846@qq.com>
2021-03-15 21:36:54 +0800

    init project

---
+++ swiftUI-first-app/swiftUI_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_first_appApp.swift
+//  swiftUI-first-app
+//
+//  Created by smilemike on 2021/3/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
