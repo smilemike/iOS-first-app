d658db576d383965829828ab538f6e18613bad22
smilemike <419854846@qq.com>
2021-03-16 20:43:06 +0800

    添加初始view

--- swiftUI-first-app/ContentView.swift
+++ swiftUI-first-app/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack {
+                Color(red: 0.5, green: 0.5, blue: 0.5)
+                Text("Hello, world!")
+                    .padding()
+            }
+            VStack {
+                Color(red: 0.5, green: 0.5, blue: 0.5)
+                Text("Hello, world!")
+                    .padding()
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
